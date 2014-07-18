db0194fff9f3bf606839be1ba3fa7a5c9519f5cd
Alicia Hotovec-Ellis <ahotovec@gmail.com>
2014-07-18 16:43:45 -0700

    added new AIC-based picker to (hopefully) improve trigger times

--- redpy/trigger.py
+++ redpy/trigger.py
@@ -2,6 +2,7 @@ from obspy import UTCDateTime
 from obspy.fdsn import Client
 from obspy.core.trace import Trace
 from obspy.signal.trigger import classicSTALTA, triggerOnset
+import numpy as np
 
 def getIRIS(date, opt, nsec=86400):
 
@@ -46,19 +47,66 @@ def trigger(st, opt):
 
     cft = classicSTALTA(tr.data, opt.swin*opt.samprate, opt.lwin*opt.samprate)
     on_off = triggerOnset(cft, opt.trigon, opt.trigoff)
+    
+    pick = np.zeros([len(on_off),1])
+    for n in range(len(pick)):
+        pick[n] = aicpick(st, on_off[n, 0], opt)
 
     ttime = 0
     for n in range(len(on_off)):
         if on_off[n, 0] > ttime + opt.mintrig*opt.samprate:
-            if ttime is 0 and on_off[n, 0] > ttime + opt.ptrig*opt.samprate:
-                ttime = on_off[n, 0]
+            if ttime is 0 and pick[n] > ttime + opt.ptrig*opt.samprate:
+                ttime = pick[n]
                 trigs = st.slice(t - opt.ptrig + ttime/opt.samprate,
                                  t + opt.atrig + ttime/opt.samprate)
             else:
-                ttime = on_off[n,0]
+                ttime = pick[n]
                 if ttime < len(tr.data) - (opt.atrig + opt.ptrig)*opt.samprate:
                     trigs = trigs.append(tr.slice(
                         t - opt.ptrig + ttime/opt.samprate,
                         t + opt.atrig + ttime/opt.samprate))
 
     return trigs
+    
+
+def aicpick(st, initialTrigger, opt):
+    
+    """
+    An autopicker to (hopefully) improve consistency in triggering
+    
+    st: OBSPy stream of data containing trigger
+    initialTrigger: initial guess at trigger time (in number of samples into stream)
+    opt: Options object describing station/run parameters
+    
+    Returns updated trigger time
+    
+    AIC stands for Akaike Information Criterion. This code is based on the formula in
+    Zhang, Thurber, and Rowe [2003] (originally from Maeda [1985]) to calculate AIC
+    directly from the waveform. It is a purely statistical picker, and the minimum of
+    the AIC corresponds to where one can divide the signal into two different parts (in
+    this case, noise followed by signal).
+    """
+    
+    t = st[0].stats.starttime   
+    x0 = st.slice(t - opt.ptrig/2 + initialTrigger/opt.samprate,
+                  t + opt.ptrig/2 + initialTrigger/opt.samprate)
+    x = x0[0].data
+    nsamp = int(opt.ptrig*opt.samprate)
+    
+    AIC = np.zeros([nsamp,1])
+        
+    for k in range(nsamp):
+            
+        # Calculate the Akaike Information Criteria
+        var1 = np.var(x[0:k+1])
+        var2 = np.var(x[k:nsamp+1])
+        
+        if var1 == 0 or var2 == 0:
+            AIC[k] = np.NaN
+        else:
+            AIC[k] = (k+1)*np.log10(var1) + (nsamp-k)*np.log10(var2)
+            
+    # Pad 10 samples on either end to prevent encountering edge effects
+    picksamp = np.argmin(AIC[10:nsamp-10]) + initialTrigger - nsamp/2
+                
+    return picksamp
